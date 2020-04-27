27dfbd16ce300987f9150c9d42f0073fb79a0ff6
digital032584 <tonymyers@hotmail.com>
2020-04-27 15:30:17 +0100

    DEP-304: fix unit tests

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -15,7 +15,9 @@ object AppDependencies {
   val test = Seq(
     "uk.gov.hmrc"             %% "bootstrap-play-26"        % "1.7.0" % Test classifier "tests",
     "org.scalatest"           %% "scalatest"                % "3.0.8"                 % "test",
+    "org.scalacheck"          %% "scalacheck"               % "1.13.4"                % "test",
     "org.jsoup"               %  "jsoup"                    % "1.10.2"                % "test",
+    "org.mockito"             % "mockito-all"               % "1.10.19"               % "test",
     "com.typesafe.play"       %% "play-test"                % current                 % "test",
     "org.pegdown"             %  "pegdown"                  % "1.6.0"                 % "test, it",
     "org.scalatestplus.play"  %% "scalatestplus-play"       % "3.1.2"                 % "test, it"
