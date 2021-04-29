317604b1dc8605270e4afaa029707088e7fc89a0
Jay Nabonne <jaynabonne@gmail.com>
2021-04-29 10:35:51 +0100

    DEP-1766: Upgrade to Play 2.8. Fix warnings and deprecated settings.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,21 +2,18 @@ import sbt.{ModuleID, _}
 
 object AppDependencies {
 
-  val compile = Seq(
-
-//    "uk.gov.hmrc"             %% "govuk-template"               % "5.61.0-play-27",
-//    "uk.gov.hmrc"             %% "play-ui"                      % "8.20.0-play-27",
-    "uk.gov.hmrc"             %% "bootstrap-frontend-play-27"   % "3.3.0"
+  val compile: Seq[ModuleID] = Seq(
+    "uk.gov.hmrc"             %% "bootstrap-frontend-play-28"   % "4.3.0"
   )
 
-  val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-frontend-play-27"   % "3.3.0"     % "test",
+  val test: Seq[ModuleID] = Seq(
+    "uk.gov.hmrc"             %% "bootstrap-frontend-play-28"   % "4.3.0"     % "test",
     "org.scalatest"           %% "scalatest"                    % "3.0.8"     % "test",
     "org.scalacheck"          %% "scalacheck"                   % "1.13.4"    % "test",
     "org.jsoup"               %  "jsoup"                        % "1.10.2"    % "test",
     "org.mockito"             % "mockito-all"                   % "1.10.19"   % "test",
     "org.pegdown"             %  "pegdown"                    % "1.6.0"       % "test, it",
-    "org.scalatestplus.play"  %% "scalatestplus-play"         % "4.0.3"       % "test, it"
+    "org.scalatestplus.play"  %% "scalatestplus-play"         % "5.0.0"       % "test, it"
   )
 
   val all: Seq[ModuleID] = compile ++ test
