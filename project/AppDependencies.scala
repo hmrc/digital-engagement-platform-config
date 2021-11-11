3f97dabc9317f7e0b24d8a80c670165399ee0b3b
pratikrana06 <63359242+pratikrana06@users.noreply.github.com>
2021-11-11 14:34:32 +0000

    [DEP-2275][PR]:Library and Plugin Upgrades

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,13 +3,13 @@ import sbt.{ModuleID, _}
 object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-frontend-play-28"   % "5.0.0"
+    "uk.gov.hmrc"             %% "bootstrap-frontend-play-28"   % "5.16.0"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "org.scalatest"           %% "scalatest"                    % "3.0.8"     % "test",
-    "org.scalacheck"          %% "scalacheck"                   % "1.13.4"    % "test",
-    "org.jsoup"               %  "jsoup"                        % "1.10.2"    % "test",
+    "org.scalatest"           %% "scalatest"                    % "3.0.9"     % "test",
+    "org.scalacheck"          %% "scalacheck"                   % "1.15.4"    % "test",
+    "org.jsoup"               %  "jsoup"                        % "1.14.3"    % "test",
     "org.mockito"             %  "mockito-all"                  % "1.10.19"   % "test",
     "org.pegdown"             %  "pegdown"                      % "1.6.0"     % "test",
     "org.scalatestplus.play"  %% "scalatestplus-play"           % "5.0.0"     % "test"
