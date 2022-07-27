007bded4993a67ef438b00acaf67b39409fff425
James Forster <14073802+jameshforster@users.noreply.github.com>
2022-07-27 09:09:27 +0100

    DEP-3452 Updated bootstrap dependency.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,7 @@ import sbt.{ModuleID, _}
 object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-frontend-play-28"   % "6.3.0"
+    "uk.gov.hmrc"             %% "bootstrap-frontend-play-28"   % "6.4.0"
   )
 
   val test: Seq[ModuleID] = Seq(
