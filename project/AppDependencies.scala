1d9e02d714aaf1e26e3f7624cdcf6647f57c7345
Michael Sweeton <34985534+machinesleet@users.noreply.github.com>
2022-07-29 16:02:31 +0100

    DEP-3452 update bootstrap-frontend-play-28 (#33)
    * DEP-3452 update bootstrap-frontend-play-28
    
    * DEP-3452 update sbt-auto-build plugin
    
    * DEP-3452 Updated bootstrap dependency.
    
    Co-authored-by: James Forster <14073802+jameshforster@users.noreply.github.com>

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,7 @@ import sbt.{ModuleID, _}
 object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-frontend-play-28"   % "5.24.0"
+    "uk.gov.hmrc"             %% "bootstrap-frontend-play-28"   % "6.4.0"
   )
 
   val test: Seq[ModuleID] = Seq(
